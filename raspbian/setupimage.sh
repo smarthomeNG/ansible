4a2e27bd40f468536176e621cc60be417a4f61c2
onkelandy <onkelandy@hotmail.com>
2020-04-06 14:32:11 +0200

    update ansible yaml files

--- raspbian/setupimage.sh
+++ raspbian/setupimage.sh
@@ -1,4 +1,5 @@
-#ansible-playbook 01_Tweaks.yml
+#!/bin/bash
+ansible-playbook 01_Tweaks.yml
 ansible-playbook 02_Python_Packages.yml
 ansible-playbook 03_knxd.yml
 ansible-playbook 04_nginx.yml
@@ -16,8 +17,9 @@ ansible-playbook 15_openvpn.yml
 ansible-playbook 16_freeradius.yml
 ansible-playbook 17_watchdog.yml
 ansible-playbook 18_SQL.yml
-ansible-playbook 19_xtrabackup.yml
+ansible-playbook 19_mariadb_backup.yml
 ansible-playbook 20_mosquitto.yml
 ansible-playbook 21_ansible.yml
 ansible-playbook 22_setups.yml
 ansible-playbook 23_fail2ban.yml
+ansible-playbook 24_nodered.yml
