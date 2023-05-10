298b8f7299914a80ce90b31e9f70126c52ff9b2d
Onkel Andy <onkelandy@hotmail.com>
2023-05-10 10:24:11 +0200

    fix mariadb Backup permissions and script

--- raspbian/executables/mysql_backup.sh
+++ raspbian/executables/mysql_backup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #
 # Simple cron script to create backup of SQL databases
-
 RUNBACKUPS=False
-if [[ $RUNBACKUPS == False ]]; then
-  exit 0
-fi
 
 adddate() {
     while IFS= read -r line; do
@@ -16,10 +12,12 @@ adddate() {
 export LC_ALL=C
 log_file="/var/log/mysql/mariabackup.log"
 
-sudo chmod 0644 /etc/mysql/debian.cnf
-sudo chown mysql:mysql /etc/mysql/debian.cnf
-if [[ $RUNBACKUPS == True ]] && (command -v /usr/bin/mariabackup > /dev/null 2>&1 && pgrep -x mysqld > /dev/null 2>&1 && ! pgrep mariabackup  > /dev/null 2>&1); then
-	days_of_backups=3  # Must be less than 7
+if [[ $RUNBACKUPS == False ]]; then
+  exit 0
+fi
+
+if [[ $RUNBACKUPS == True ]] && (command -v /usr/bin/mariabackup > /dev/null 2>&1 && pgrep -x mariadbd > /dev/null 2>&1 && ! pgrep mariabackup  > /dev/null 2>&1); then
+	days_of_backups=5  # Must be less than 7
 	backup_owner="mysql"
 	parent_dir="/var/backups/mysql"
 	defaults_file="/etc/mysql/debian.cnf"
@@ -36,7 +34,15 @@ if [[ $RUNBACKUPS == True ]] && (command -v /usr/bin/mariabackup > /dev/null 2>&
 
   }
   sanity_check
+  if [ ! -f "${log_file}" ]; then
+      sudo touch $log_file
+  fi
 
+  sudo chmod 0644 /etc/mysql/debian.cnf
+  sudo chown mysql:mysql /etc/mysql/debian.cnf
+  sudo chmod 0664 /var/log/mysql/mariabackup.log
+  sudo chown mysql:adm /var/log/mysql/mariabackup.log
+  echo "MySQL Backup Initializing." | adddate >> $log_file 2>&1
 	# Use this to echo to standard error
 	error () {
 		printf "%s: %s\n" "$(basename "${BASH_SOURCE}")" "${1}" | adddate >> $log_file 2>&1
@@ -86,6 +92,7 @@ if [[ $RUNBACKUPS == True ]] && (command -v /usr/bin/mariabackup > /dev/null 2>&
 		# Make sure today's backup directory is available and take the actual backup
 		mkdir -p "${todays_dir}"
 		cd /var/log/mysql/ && find "${todays_dir}" -type f -name "*.incomplete" -delete
+    echo "Starting Backup"
 		sudo mariabackup "${mariabackup_args[@]}" --target-dir="${todays_dir}" > "${todays_dir}/${backup_type}-${now}.xbstream.incomplete" 2>> $log_file
 
 		sudo mv "${todays_dir}/${backup_type}-${now}.xbstream.incomplete" "${todays_dir}/${backup_type}-${now}.xbstream"
@@ -95,7 +102,7 @@ if [[ $RUNBACKUPS == True ]] && (command -v /usr/bin/mariabackup > /dev/null 2>&
 
 	# Check success and print message
 	if tail -1 "${log_file}" | grep -q "completed OK"; then
-		printf "Backup successful!\n" | adddate >> $log_file 2>&1
+		printf "%s\n" "Backup successful!" | adddate >> $log_file 2>&1
 		printf "Backup created at %s/%s-%s.xbstream\n" "${todays_dir}" "${backup_type}" "${now}" | adddate >> $log_file 2>&1
 	else
 		error "Backup failure! Check ${log_file} for more information" | adddate >> $log_file 2>&1
